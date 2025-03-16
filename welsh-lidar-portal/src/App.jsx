07e8f96346fcf98a39f45ca53476a64299d00e7d
Charlie <c.enright@me.com>
2025-03-16 14:48:29 +0000

    Add material MUI responsive navigation

--- welsh-lidar-portal/src/App.jsx
+++ welsh-lidar-portal/src/App.jsx
@@ -1,22 +1,23 @@
 // Import Routes and Route from React Router to handle page navigation
 import { Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation"; // Import the Navigation component
 
 // Import the pages that will be displayed
 import Home from "./pages/Home";
 import About from "./pages/About";
 
-// Define the main App component
-const App = () => {
+// the main App component
+function App() {
   return (
-    <Routes>
-      {/* Route for the Home page ("/") */}
-      <Route path="/" element={<Home />} />
-
-      {/* Route for the About page ("/about") */}
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+      <Navigation /> {/* Navbar appears on all pages */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
-};
+}
 
 // Export the App component so it can be used in main.jsx
 export default App;
--- welsh-lidar-portal/src/pages/Home.jsx
+++ welsh-lidar-portal/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react' // Import React library
+import Navigation from "../components/Navigation";
+
 
 // Define the Home component (Functional Component)
 const Home = () => {
   return (
     <div>
+        <Navigation />
         <h1>The Welsh LiDAR Portal</h1>
     </div>
   )
