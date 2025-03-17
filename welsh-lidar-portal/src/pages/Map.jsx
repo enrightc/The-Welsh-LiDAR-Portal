24b305e39c91cf97f7767e51a194a93422c7312f
Charlie <c.enright@me.com>
2025-03-17 17:04:07 +0000

    Add Feat: Esri World map and LiDAR imagery added to map layers

--- welsh-lidar-portal/src/pages/Map.jsx
+++ welsh-lidar-portal/src/pages/Map.jsx
@@ -4,21 +4,52 @@ import React from 'react'
 import {
     MapContainer,
     TileLayer,
+    WMSTileLayer,
     useMap,
     Marker,
     Popup,
+    LayersControl,
   } from 'react-leaflet'
 
 const Map = () => {
   return (
-        <div style={{ height: "100vh" }}>
-            <MapContainer center={[52.1307, -3.7837]} zoom={8.5} scrollWheelZoom={true}>
+    <div style={{ height: "100vh", marginTop: "64px" }}>
+      <MapContainer center={[52.1307, -3.7837]} zoom={8.5} scrollWheelZoom={true}>
+
+        <LayersControl position="topright">
+          
+          {/* Default OpenStreetMap Layer */}
+          <LayersControl.BaseLayer checked name="OpenStreetMap">
             <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            </MapContainer>
-        </div>
+          </LayersControl.BaseLayer>
+
+          {/* Esri World Imagery Satellite Layer */}
+          <LayersControl.BaseLayer name="Esri World Imagery">
+            <TileLayer
+              url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+              attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+            />
+          </LayersControl.BaseLayer>
+
+          {/* WMS LiDAR DSM Hillshade Layer */}
+          <LayersControl.Overlay name="LiDAR DSM Hillshade">
+            <WMSTileLayer
+              url="https://datamap.gov.wales/geoserver/ows?"
+              layers="geonode:wales_lidar_dsm_1m_hillshade_cog"
+              format="image/png"
+              transparent={true}
+              opacity={1}
+              attribution='&copy; <a href="https://datamap.gov.wales/">DataMap Wales</a>'
+            />
+          </LayersControl.Overlay>
+
+        </LayersControl>
+
+      </MapContainer>
+    </div>
     )
 }
 
